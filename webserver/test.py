d7bc7d544192195a8c350773234943cea514177c
Mike Wilkerson <mike.wilkerson@willowtreeapps.com>
2015-01-17 16:26:04 -0500

    Getting blank index file up to github so I can pull the URL

--- webserver/test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-print "AWS_ACCESS_KEY_ID is {}".format(
-    os.environ.get('AWS_ACCESS_KEY_ID') or "not set"
-)
-print "AWS_SECRET_ACCESS_KEY is {}".format(
-    os.environ.get('AWS_SECRET_ACCESS_KEY') or "not set"
-)
